7a5493a529f850630590a35b294cd7979a5c9911
onereadytwo <estudioprogramador.jose@gmail.com>
2023-11-07 18:24:13 -0300

    actualizacion version2

---
+++ app/blog/layout.jsx
@@ -0,0 +1,27 @@
+import { Inter } from 'next/font/google'
+// import './globals.css'
+import Script from 'next/script'
+import Navigation from './components/Navigation.jsx'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: ' blog | como ser programador junior | que computadora Comprar   ',
+  description: 'fundamentos de la programación, elige un lenguaje de programación que te interese,',
+}
+
+
+export default function RootLayout({ children }) {
+  return (
+    
+   
+    
+      <>
+        
+       {children}
+
+       
+       </>
+    
+  )
+}
