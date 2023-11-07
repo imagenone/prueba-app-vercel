ef7a71b3dc3e2a9603c752b31f2944b04ea7526e
onereadytwo <estudioprogramador.jose@gmail.com>
2023-11-07 17:52:00 -0300

    cambio en la pagina

---
+++ app/blog/como-ser-programador-junior/page.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styles from '../../stylehome.module.css'
+
+export default function Page() {
+  return (
+    <div style={{  padding: "1px", textAlign: "justify", textJustify: "inter-word"}}>
+      <div className={styles.backgroundImage_3}>
+                  <h1 className={`fs-5 ${styles.colorText}`} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+                    
+                  
+                    </h1>
+                  </div>
+      <h1 className='text-center'>Pasos Iniciales para Convertirte en un Programador Junior</h1>
+      <p className='p-3' >
+        La programación es un mundo apasionante y en constante evolución que ofrece oportunidades ilimitadas para aquellos que desean adentrarse en él. Desde el desarrollo de aplicaciones y sitios web hasta la automatización de tareas cotidianas, la programación desempeña un papel fundamental en la resolución de problemas y la creación de soluciones innovadoras. Si estás interesado en convertirte en un programador junior, estás dando el primer paso hacia un viaje de aprendizaje continuo y posibilidades infinitas. Esta guía te proporcionará los pasos iniciales esenciales que debes seguir para construir una base sólida en programación y avanzar hacia una carrera prometedora. Ya sea que seas un principiante absoluto o alguien que busca consolidar sus conocimientos, estos pasos te proporcionarán las bases necesarias para alcanzar tus metas en el emocionante mundo de la programación.
+      </p>
+      <ol>
+        <li>
+          <strong>Aprender los Fundamentos de la Programación</strong>: Comienza por entender conceptos fundamentales como variables, condicionales, bucles y estructuras de datos.
+        </li>
+        <li>
+          <strong>Domina un Lenguaje de Programación</strong>: Elige un lenguaje que se adapte a tus objetivos y profundiza en su sintaxis y características.
+        </li>
+        <li>
+          <strong>Explora Herramientas de Desarrollo</strong>: Familiarízate con IDEs y editores de código que te ayudarán a escribir y depurar tu código.
+        </li>
+        <li>
+          <strong>Resuelve Problemas</strong>: Practica resolviendo desafíos de programación en línea para mejorar tus habilidades lógicas y algorítmicas.
+        </li>
+        <li>
+          <strong>Crea Proyectos Personales</strong>: Comienza con proyectos simples y avanza hacia proyectos más complejos a medida que adquieras experiencia.
+        </li>
+        <li>
+          <strong>Comprende las Estructuras de Datos y Algoritmos</strong>: Estudia las estructuras de datos y algoritmos básicos.
+        </li>
+        <li>
+          <strong>Aprende de la Comunidad</strong>: Únete a comunidades en línea para hacer preguntas, compartir conocimientos y colaborar en proyectos.
+        </li>
+        <li>
+          <strong>Construye un Portafolio</strong>: Crea un portafolio en línea para mostrar tus proyectos y habilidades.
+        </li>
+        <li>
+          <strong>Busca Oportunidades de Empleo o Pasantías</strong>: Busca empleo como programador junior o pasantías para ganar experiencia.
+        </li>
+        <li>
+          <strong>Aprende de la Experiencia</strong>: Aprovecha cada oportunidad para aprender de tus errores y experiencias.
+        </li>
+      </ol>
+      <p>
+        Recuerda que la paciencia y la práctica son clave en el camino para convertirte en un programador junior. ¡Buena suerte en tu viaje como programador!
+      </p>
+    </div>
+  );
+}
---
+++ app/blog/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../stylehome.module.css'
+export default function BlogPage() {
+  return (      
+    <div className='container border ' style={{ padding: "10px", textAlign: "justify", textJustify: "inter-word"}}>
+<h1>blogpage</h1>
+    </div>
+  )
+}
+  
